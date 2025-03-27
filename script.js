42e2b3520295e3762166ee22f6d59f88469cb771
davosab <davidsabau72@gmail.com>
2025-03-27 22:17:28 +0200

    sun

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+let sunX = 400;
+let sunY = 200;
+let sunD = 80;
+let sunR = sunD/2;
+let sunGap = 4;
+
+let triBase = 22;
+let triHeight = 30;
+
 function setup() {
   createCanvas(800, 400);
+  angleMode(DEGREES);
 }
 function draw() {
   background("#010136");
+
+  // sun
+  fill("yellow");
+  circle(sunX, sunY, sunD);
+  // rays
+  translate(sunX, sunY);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(45);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(90);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(135);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(180);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(225);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(270);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+  rotate(315);
+  triangle(sunR + sunGap, -triBase/2, sunR + sunGap, triBase/2, sunR + sunGap + triHeight, 0);
+
+  resetMatrix();
+  
+
 }
